e6bf486c601106e208f225f55f36850c08307271
Daniel Rusch <daniel-rusch@hotmail.com>
2022-09-25 19:39:20 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready
+
+function cadastraremail() {
+  let email = document.getElementById("campoemail").value;
+  alert("O e-mail " + email + " foi cadastrado com sucesso!");
+}
+
+function adicionarcarrinho() {
+  alert("O item foi adicionado com sucesso ao carrinho de compras!");
+}
+
+function login() {
+  alert("Bem vindo!");
+}
